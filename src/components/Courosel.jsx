bab0bc7478a64abf53d9640707b2e1f75bea7934
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-26 12:17:19 +0300

    Updated code

--- src/components/Courosel.jsx
+++ src/components/Courosel.jsx
@@ -1,92 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./stylings/Courosel.css"; // import external CSS
 
 const Courosel = () => {
   return (
-    <div className="background">
-      <div className="row align-items-center justify-content-between p-3">
-        {/* Left Text */}
-        <div className="col-md-4 text-white icon">
-          <span>
-            <h1>Our</h1>
-          </span>
-          <span>
-            <h1>Bakery </h1>
-          </span>
-          <span>
-            <h1>start</h1>
-          </span>
-          <span>
-            <h1>OFF%5</h1>
-          </span>
-          <span>We would like You to enjoy our </span>
-          <br />
-          <span>fine made delicacies</span>
-          <br />
-          <br />
-          <br />
-          <h1 className="rainbow-text background-home">Lexxy pastry</h1>
-        </div>
-          {/* Carousel */}
-          <div className="carousel-body">
-            <div className="carousel-container">
-              <div className="carousel-card">
-                <h3 className="carousel-title">Our Shop</h3>
-                <div className="carousel-bar">
-                  <div className="carousel-emptybar"></div>
-                  <div className="carousel-filledbar"></div>
-                </div>
-                <div className="carousel-circle">
-                  <img src="images/cake2.avif" alt="" className="car-img" />
-                </div>
-              </div>
+    <section className="hero-section">
+      {/* Left Text Section */}
+      <div className="hero-text">
+        <h1>Our</h1>
+        <h1>Bakery</h1>
+        <h1>start</h1>
+        <h1>OFF%5</h1>
+        <p>We would like You to enjoy our fine made delicacies</p>
+        <h1 className="rainbow-text">Lexxy pastry</h1>
+      </div>
 
-              <div className="carousel-card">
-                <h3 className="carousel-title">Other pastries</h3>
-                <div className="carousel-bar">
-                  <div className="carousel-emptybar"></div>
-                  <div className="carousel-filledbar"></div>
-                </div>
-                <div className="carousel-circle">
-                  <img src="images/cakes.jpeg" alt="" className="car-img" />
-                </div>
-              </div>
-
-              <div className="carousel-card">
-                <h3 className="carousel-title">Pies</h3>
-                <div className="carousel-bar">
-                  <div className="carousel-emptybar"></div>
-                  <div className="carousel-filledbar"></div>
-                </div>
-                <div className="carousel-circle">
-                  <img src="images/pies1.jpg" alt="" className="car-img" />
-                </div>
-              </div>
-
-              <div className="carousel-card">
-                <h3 className="carousel-title">Delicacies</h3>
-                <div className="carousel-bar">
-                  <div className="carousel-emptybar"></div>
-                  <div className="carousel-filledbar"></div>
-                </div>
-                <div className="carousel-circle">
-                  <img
-                    src="images/otherpastry.jpeg"
-                    alt=""
-                    className="car-img"
-                  />
-                </div>
-              </div>
-
-             
-              
+      {/* Right Carousel Section */}
+      <div className="carousel-body">
+        <div className="carousel-container">
+          <div className="carousel-card">
+            <h3 className="carousel-title">Our Shop</h3>
+            <div className="carousel-bar">
+              <div className="carousel-emptybar"></div>
+              <div className="carousel-filledbar"></div>
+            </div>
+            <div className="carousel-circle">
+              <img src="images/cake2.avif" alt="cake" className="car-img" />
             </div>
           </div>
-          {/* End Carousel */}
+
+          <div className="carousel-card">
+            <h3 className="carousel-title">Other pastries</h3>
+            <div className="carousel-bar">
+              <div className="carousel-emptybar"></div>
+              <div className="carousel-filledbar"></div>
+            </div>
+            <div className="carousel-circle">
+              <img src="images/cakes.jpeg" alt="cakes" className="car-img" />
+            </div>
+          </div>
+
+          <div className="carousel-card">
+            <h3 className="carousel-title">Pies</h3>
+            <div className="carousel-bar">
+              <div className="carousel-emptybar"></div>
+              <div className="carousel-filledbar"></div>
+            </div>
+            <div className="carousel-circle">
+              <img src="images/pies1.jpg" alt="pies" className="car-img" />
+            </div>
+          </div>
+
+          <div className="carousel-card">
+            <h3 className="carousel-title">Delicacies</h3>
+            <div className="carousel-bar">
+              <div className="carousel-emptybar"></div>
+              <div className="carousel-filledbar"></div>
+            </div>
+            <div className="carousel-circle">
+              <img
+                src="images/otherpastry.jpeg"
+                alt="delicacies"
+                className="car-img"
+              />
+            </div>
+          </div>
         </div>
       </div>
-    
+    </section>
   );
 };
 
